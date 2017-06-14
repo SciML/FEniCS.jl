a220e5d241ab646415d337ff7bf351dee3b606d8
Yiannis Simillides <yiannis.simillides.14@ucl.ac.uk>
2017-06-14 14:05:57 +0100

    Add files via upload
    Created the jmesh.jl file which contains the inbuilt meshes from FEniCS, along with the skeleton for the mesh type. Only basic functions are currently implemented for the mesh type, but the majority of them are currently being added

--- src/FEniCS.jl
+++ src/FEniCS.jl
@@ -1,7 +1,29 @@
 __precompile__()
 
 module FEniCS
+using PyCall
+@pyimport fenics
+#the below code is an adaptation of aleadev.FEniCS.jl
 
-# package code goes here
+import Base: size, length, show, *, +, -, Function,repr
+abstract fenicsobject #creates placeholder for the fenicsobject type
+fenicspycall(object::fenicsobject, func::Union{Symbol,String}, args...) = object.pyobject[func](args...)
 
-end # module
+macro fenicsclass(name::Symbol, base1::Symbol=:fenicsobject, base2::Symbol=:Any, base3::Symbol=:Any)
+  impl = Symbol(name, "Impl")
+  quote
+    abstract $name <: $base1, $base2, $base3
+    immutable $impl <: $name
+      pyobject::PyObject
+    end
+    $(esc(name))(pyobject::PyObject) = $impl(pyobject)
+  end
+end
+
+str(obj::fenicsobject) = fenicspycall(obj, :__str__)
+repr(obj::fenicsobject) = fenicspycall(obj, :__repr__)
+show(io::IO, obj::fenicsobject) = show(io, str(obj))
+export str, repr
+#the code from aleadev.fenics.jl has ended. The code underneath should be moved to the mesh.jl file before being uploaded
+include("jmesh.jl")
+end# module
