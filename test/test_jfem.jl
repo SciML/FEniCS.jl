43f76414f5da4128445070d941758fd05dcb502c
Yiannis Simillides <yiannis.simillides.14@ucl.ac.uk>
2017-07-27 10:26:50 +0100

    Update test_jfem.jl

--- test/test_jfem.jl
+++ test/test_jfem.jl
@@ -8,8 +8,13 @@ v = TestFunction(V)
 f = Constant(-6.0)
 
 A_1 = inner(grad(u),grad(v))*dx
-B = assemble(A_1)
+A_2 = dot(grad(u),grad(v))*dx
+A_3 = cross(grad(u),grad(v))
+A_4 = outer(grad(u),grad(v))
 ng = nabla_grad(u)
+
+B_1 = assemble(A_1)
+B_2 = assemble(A_2)
 
 measure_x = dx
 measure_s = ds
