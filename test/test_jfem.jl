0f6ddb35bf7a7af9681f092be2e52e1b98d10b12
Yiannis Simillides <yiannis.simillides.14@ucl.ac.uk>
2017-08-24 18:31:30 +0100

    Add files via upload

--- test/test_jfem.jl
+++ test/test_jfem.jl
@@ -1,11 +1,24 @@
 
 using FEniCS
+using PyCall
+@pyimport fenics
 
 mesh = UnitSquareMesh(8, 8)
 V = FunctionSpace(mesh, "P", 1)
+u_D = Expression("1+x[0]*x[0]+2*x[1]*x[1]",degree=2 )
+bc = DirichletBC(V,u_D, "on_boundary")
 u = TrialFunction(V)
 v = TestFunction(V)
-f = Constant(-6.0)
+f = FEniCS.Constant(-6.0)
+a = dot(grad(u),grad(v))*dx
+L = f*v*dx
+U = FEniCS.Function(V)
+lvsolve(a,L,U,bc)
+errornorm(u_D,U,norm="L2")
+file_sol= File("FEniCS.jl/test_jfemsol.pvd")
+file_sol << U.pyobject
+
+
 
 A_1 = inner(grad(u),grad(v))*dx
 A_2 = dot(grad(u),grad(v))*dx
@@ -23,5 +36,31 @@ measure_P = dP
 
 meas_add1 = dx+ds
 meas_add2 = dS+dP
+
+#matrix_U = assemble(U)
+matrix_L = assemble(L)
+vector_U = vector(U)
+genericvector_U = Vector(vector_U)
+
+T = 2.0            # final time
+num_steps = 10     # number of time steps
+dt = T / num_steps # time step size
+alpha = 3          # parameter alpha
+beta = 1.2         # parameter beta
+
+u_D = Expression(fenics.Expression("1 + x[0]*x[0] + alpha*x[1]*x[1] + beta*t",
+                 degree=2, alpha=alpha, beta=beta, t=0))
+
+
+u_n = interpolate(u_D, V)
+u_1 = TrialFunction(V)
+v_1 = TestFunction(V)
+f_1= Constant(beta - 2 - 2*alpha)
+
+F = u*v*dx + dt*dot(grad(u), grad(v))*dx - (u_n + dt*f)*v*dx
+
+A_1, L_1 = lhs(F), rhs(F)
+
+
 
 true
