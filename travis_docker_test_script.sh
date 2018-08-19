f62834e8fbd4804069a0ba5c59bfaa4859dea926
Yiannis Simillides <yiannis.simillides.14@ucl.ac.uk>
2018-08-19 21:46:15 +0100

    Update travis_docker_test_script.sh

--- travis_docker_test_script.sh
+++ travis_docker_test_script.sh
@@ -9,10 +9,10 @@ cd /mnt && if [[ -a .git/shallow ]]; then git fetch --unshallow; fi
 
 # run tests
 #julia -e "Pkg.clone(\"https://github.com/JuliaPy/PyCall.jl\"); ENV[\"PYTHON\"] = \"/usr/bin/python3\"; Pkg.build(\"PyCall\")"
-julia -e "Pkg.clone(\"/mnt/\", \"$PKGNAME\"); Pkg.build(\"$PKGNAME\"); Pkg.test(\"$PKGNAME\"; coverage=true)"
+julia -e "import Pkg; Pkg.clone(\"/mnt/\", \"$PKGNAME\"); Pkg.build(\"$PKGNAME\"); Pkg.test(\"$PKGNAME\"; coverage=true)"
 TEST_EXIT=$?                    # return with this
 
 # save coverage results back to host
-PKGDIR=`julia -e "print(Pkg.dir(\"$PKGNAME\"))"`
+PKGDIR=`julia -e "import Pkg; print(Pkg.dir(\"$PKGNAME\"))"`
 rsync --no-o --no-g --no-perms -mav --include="*/" --include="*.cov" --exclude="*" $PKGDIR/ /mnt/
 exit $TEST_EXIT
